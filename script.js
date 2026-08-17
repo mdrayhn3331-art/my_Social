dadeb6a29f12da2a91ce8f5ca69b9d86b4385de2
mdrayhn3331-art <mdrayhn3331@gmail.com>
2026-08-17 18:11:47 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,52 @@
 // Loading Screen
 window.addEventListener("load", () => {
+
     setTimeout(() => {
+
         const loader = document.querySelector(".loader");
+
         loader.style.opacity = "0";
+        loader.style.transition = "0.8s";
         loader.style.pointerEvents = "none";
 
         setTimeout(() => {
             loader.style.display = "none";
-        }, 600);
-    }, 1200);
+        }, 800);
+
+    }, 4000);
+
 });
+
 
 // Copy Link Button
 function copyLink() {
+
     navigator.clipboard.writeText(window.location.href)
-        .then(() => {
-            alert("✅ Link copied successfully!");
-        })
-        .catch(() => {
-            alert("❌ Copy failed.");
-        });
+
+    .then(() => {
+        alert("✅ Link copied successfully!");
+    })
+
+    .catch(() => {
+        alert("❌ Copy failed.");
+    });
+
 }
+
 
 // Button Click Animation
 document.querySelectorAll("a").forEach(btn => {
+
     btn.addEventListener("click", function () {
+
         this.style.transform = "scale(0.95)";
+
         setTimeout(() => {
+
             this.style.transform = "";
-        }, 150);
+
+        },150);
+
     });
+
 });
